542665a8c584ee8d5280870967f85dc9f2b437f5
Cornelius Köpp <cornelius.koepp@fhdw.de>
2024-07-31 22:29:50 +0200

    Untested Draft: Get Current Temp and Rain from OpenMeteo

--- src/OpenMeteoChannel.cpp
+++ src/OpenMeteoChannel.cpp
@@ -1,5 +1,5 @@
 #include "OpenMeteoChannel.h"
-#define OpenMeteoUrl "https://api.openweathermap.org/data/3.0/onecall?units=metric&lang=de&exclude=minutely,alerts"
+#define OpenMeteoUrl "https://api.open-meteo.com/v1/forecast"
 
 OpenMeteoChannel::OpenMeteoChannel(uint8_t index)
     : BaseWeatherChannel(index)
@@ -13,13 +13,23 @@ const std::string OpenMeteoChannel::name()
 
 int16_t OpenMeteoChannel::fillWeather(CurrentWheatherData& currentWeather, ForecastDayWheatherData& todayWeather, ForecastDayWheatherData& tomorrowWeather, ForecastHourWheatherData& hour1Weather, ForecastHourWheatherData& hour2Weather)
 {
+    // TODO check using unixtimestamp format
+    // TODO check using csv-result
+
     String url = OpenMeteoUrl;
-    url += "&appid=";
-    url += (const char*)ParamIW_APIKey;
-    url += "&lat=";
+    // TODO add optional key, check combination with other endpoint required
+    // url += "&appid=";
+    // url += (const char*)ParamIW_APIKey;
+    // TODO set timezone
+    url += "?timezone=Europe%2FBerlin";
+    url += "&latitude=";
     url += ParamIW_CHWeatherLocationType == 0 ? ParamBASE_Latitude : ParamIW_CHLatitude;
-    url += "&lon=";
+    url += "&longitude=";
     url += ParamIW_CHWeatherLocationType == 0 ? ParamBASE_Longitude : ParamIW_CHLongitude;
+    url += "&current=";
+    // TODO select based on configuration?
+    url += "temperature_2m,rain";
+
     logDebugP("Call: %s", url.c_str());
     HTTPClient http;
 #ifdef ARDUINO_ARCH_RP2040   
@@ -38,6 +48,7 @@ int16_t OpenMeteoChannel::fillWeather(CurrentWheatherData& currentWeather, Forec
     JsonObject current = doc["current"];
     fillForecast(current, currentWeather);
   
+    /*
     JsonArray daily = doc["daily"];
     JsonObject today = daily[0];
     fillForecast(today, todayWeather);
@@ -49,35 +60,40 @@ int16_t OpenMeteoChannel::fillWeather(CurrentWheatherData& currentWeather, Forec
     fillForecast(hour1, hour1Weather);
     JsonObject hour2 = hourly[2];
     fillForecast(hour2, hour2Weather);
+    */
 
     return httpStatus;
 }
 
 void OpenMeteoChannel::fillForecast(JsonObject& json, CurrentWheatherData& wheater)
 {
-    wheater.temperature = json["temp"];                  // 22.34
-    wheater.temperatureFeelsLike = json["feels_like"];   // 21.95
-    wheater.humidity = json["humidity"];                 // 69
-    wheater.pressure = json["pressure"];                 // 1006
-    wheater.windSpeed = 3.6 * (float)json["wind_speed"]; // 69
-    wheater.windGust = 3.6 * (float)json["wind_gust"];   // 69
-    wheater.windDirection = json["wind_deg"];            // 70
-    JsonObject rainObject = json["rain"];
-    wheater.rain = rainObject ? (float) rainObject["1h"] : (float)0; // 2.5
-    JsonObject snowObject = json["snow"];
-    wheater.snow = snowObject ? (float) snowObject["1h"] : (float)0; // 2.5
-    wheater.uvi = json["uvi"];                                 // 6.29
-    wheater.clouds = json["clouds"];                           // 40
+    // TODO fill with data from OpenMeteo
+    wheater.temperature = json["temperature_2m"];        // 22.34
+    wheater.temperatureFeelsLike = 20.0;                 // 21.95
+    wheater.humidity = 70.0;                             // 69
+    wheater.pressure = 1000.0;                           // 1006
+    wheater.windSpeed = 50;                              // 3.6 * (float)json["wind_speed"]; // 69
+    wheater.windGust = 50;                               // 3.6 * (float)json["wind_gust"];   // 69
+    wheater.windDirection = 180;                         // 70
+    wheater.rain = json["rain"]                          // 2.5
+    wheater.snow = 0.0;                                  // 2.5
+    wheater.uvi = 0.0                                    // 6.29
+    wheater.clouds = 50                                  // 40
 }
 
 void OpenMeteoChannel::fillForecast(JsonObject& json, ForecastHourWheatherData& wheater)
 {
+    // TODO fill with data from OpenMeteo
+    /*
     fillForecast(json, (CurrentWheatherData&) wheater);
     wheater.probabilityOfPrecipitation = round(100. * (float) json["pop"]);    // 0.70
+    */
 }
 
 void OpenMeteoChannel::fillForecast(JsonObject& json, ForecastDayWheatherData& wheater)
 {
+    // TODO fill with data from OpenMeteo
+    /*
     JsonObject tempObject = json["temp"];
     wheater.temperatureDay = tempObject["day"];     // 21.95
     wheater.temperatureNight = tempObject["night"]; // 21.95
@@ -100,4 +116,5 @@ void OpenMeteoChannel::fillForecast(JsonObject& json, ForecastDayWheatherData& w
     wheater.probabilityOfPrecipitation = round(100. * (float) json["pop"]);    // 0.70
     wheater.uvi = json["uvi"];                           // 6.29
     wheater.clouds = json["clouds"];                     // 40
+    */
 }
